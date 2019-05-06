9a3eaecb8db01d4a70409c2cfebb633c2e9a4165
mjhram <mjhram2000@gmail.com>
2019-05-06 18:45:41 +0300

    DEBUG_V removal

--- src/wSerial.cpp
+++ src/wSerial.cpp
@@ -6,12 +6,12 @@ void wSerial::begin(long baud) {
 
 void wSerial::print(String tmp) {
     Serial.print(tmp);
-    DEBUG_V(tmp.c_str());
+    Debug.printf(tmp.c_str());
 }
 void wSerial::println(String tmp) {
     Serial.println(tmp);
     String tmp2 = tmp+"\n";
-    DEBUG_V(tmp2.c_str());
+    Debug.printf(tmp2.c_str());
 }
 void wSerial::write(size_t tmp) {
     Serial.write(tmp);    
@@ -22,7 +22,7 @@ void wSerial::printf(char* fmt, ...) {
 
     Serial.printf(fmt, args);    
     va_end(args);
-    DEBUG_V(fmt);
+    Debug.printf(fmt);
 }
 wSerial::wSerial(RemoteDebug& rd) : Debug(rd) {
         
